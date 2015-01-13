b3a5290793e577e985cd8e877a0b10d25908eaf8
Your Name <you@example.com>
2015-01-13 09:58:51 +0300

    fixes

--- app.js
+++ app.js
@@ -1,8 +1,21 @@
+/**
+* Software: Node chat based on sockets
+* Author: Alex Lundgren
+* Mail: alex@endem.su
+* Year: 2015
+*/
+
+
 var net = require('net'),
     connections = [];
 
+// Create Server for incoming connections
 server = net.createServer(function (socket) {
+    
+        // Define message Encoding to utf8
         socket.setEncoding('utf8');
+    
+        // Set timeout action for innactive users
         socket.setTimeout(1000*120, function () {
             for (i = 0; i < connections.length; i++) {
                 if (connections[i].address == socket.address().address) {
@@ -14,30 +27,34 @@ server = net.createServer(function (socket) {
         });
 });
 
-server.on('connection', function (socket) {
+// Actions on messages
+server.on('connection', function (socket) {    
     
     'use strict';
-    
-    var i, j;
-    
+        
+    // Add connection to connections array
     connections.push({
         socket: socket, 
         address: socket.address().address, 
         name: ''
     }); 
     
+    // Display count of connections
     socket.write(connections.length + ' people in chat! \n');
     
+    // Tell wellcome meassage for people if we connect to chat
     connections.forEach(function (connection) {
         connection.socket.write('We have new user in our chat from ip: ' + socket.address().address + '\n');
     });
 
+    // Wellcome, message for define nickname
     socket.write('Hi there, lets chat, tell me your name: ');
-       
+    
     socket.on('data', function (data) {  
-        
+        // Trim \n in new messages 
         data = String(data).trim();
         
+        // Display our message to peolpe or define nickname of in not exist
         connections.forEach(function (connection) {  
         
             if (connection.name === '') {
@@ -54,8 +71,10 @@ server.on('connection', function (socket) {
     
 });
 
+// Display errors
 server.on('erroe', function (error) {
     console.log(error);
 });
 
+// Start and bind server to 0.0.0.0:9999 to incoming connections
 server.listen(9999);
